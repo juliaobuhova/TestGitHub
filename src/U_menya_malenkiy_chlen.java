93989f4f5d1d54981b86a8b3ac2563b52758745b
Norald <pro_vlad_pr1996@i.ua>
2015-05-05 23:13:08 +0300

    1488

---
+++ src/U_menya_malenkiy_chlen.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Владислав on 05.05.2015.
+ */
+public class U_menya_malenkiy_chlen {
+    public static void main(String[] args) {
+        System.out.println("Nazvanie klassa nepravdivoe!");
+    }
+}
