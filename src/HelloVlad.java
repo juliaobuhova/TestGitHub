e2a0faadbc812f03c58319a1225b05233e8a7f4c
Julia <juliaobuhova95@gmail.com>
2015-05-05 22:59:56 +0300

    Initial commit

---
+++ src/HelloVlad.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Юлия on 05.05.2015.
+ */
+
+public class HelloVlad {
+    public void hi() {
+        System.out.println("Привет, Влад! :)");
+    }
+}
---
+++ src/Start.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Юлия on 05.05.2015.
+ */
+public class Start {
+    public static void main(String[] args) {
+        new HelloVlad().hi();
+    }
+}
